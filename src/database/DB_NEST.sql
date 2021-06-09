bf91835c1cd2046a2735dea7d7af5d41552605de
Raul Aquino <aquinoraul207@gmail.com>
2021-06-09 00:06:30 -0300

    saved DB file

--- src/database/DB_NEST.sql
+++ src/database/DB_NEST.sql
@@ -1,2 +1,5 @@
 CREATE DATABASE nestjs_study;
 USE nestjs_study;
+
+#INSERT INTO `nestjs_study`.`user` (`name`, `email`, `password`) VALUES ('Raul', 'email@gmail.com', '12345678');
+#INSERT INTO `nestjs_study`.`user` (`name`, `email`, `password`) VALUES ('Mona', 'email@gmail.com', '12345678');
