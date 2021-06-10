724fc404fadb09a13190e12ffe8b9784c79f992b
Raul Aquino <aquinoraul207@gmail.com>
2021-06-10 18:30:47 -0300

    commented hello world

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,10 +5,10 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('hello')
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  // @Get('hello')
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
 
   @Get('description')
   getDescription(): any { 
